326934590a09f862f43ec7dea97bf2d1c2e33d40
Kosh <koshatul@users.noreply.github.com>
2020-12-25 19:02:37 +1000

    fix order of execution in script for docker login

--- scripts/docker-login.sh
+++ scripts/docker-login.sh
@@ -2,14 +2,14 @@
 
 set -eo pipefail
 
-sanitize "${INPUT_USERNAME}" "username"
-sanitize "${INPUT_PASSWORD}" "password"
-
-echo "${INPUT_PASSWORD}" | docker login -u "${INPUT_USERNAME}" --password-stdin
-
 function sanitize() {
     if [ -z "${1}" ]; then
         >&2 echo "Unable to find the ${2}. Did you set with.${2}?"
         exit 1
     fi
 }
+
+sanitize "${INPUT_USERNAME}" "username"
+sanitize "${INPUT_PASSWORD}" "password"
+
+echo "${INPUT_PASSWORD}" | docker login -u "${INPUT_USERNAME}" --password-stdin
